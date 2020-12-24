cb0ddfee397d179b02418d348a25e13567347f43
pmanlig <patrik@manlig.org>
2020-12-24 17:12:46 +0100

    Optimized

--- src/Solvers/S23.js
+++ src/Solvers/S23.js
@@ -2,78 +2,64 @@
 import Solver from './Solver';
 
 class Cups {
-	constructor(txt) {
-		this.cups = txt.split('').map(i => parseInt(i, 10));
-		this.current = 0;
+	constructor(txt, n) {
+		this.cups = [];
+		let labels = txt.split('').map(i => parseInt(i, 10));
+		for (let i = 0; i < labels.length; i++) { this.cups[labels[i]] = labels[(i + 1) % labels.length]; }
+		if (n !== undefined) {
+			this.cups[n] = labels[0];
+			this.cups[labels[labels.length - 1]] = labels.length + 1;
+			for (let i = labels.length + 1; i < n; i++) {
+				this.cups[i] = i + 1;
+			}
+		}
+		this.current = labels[0];
 		this.shuffles = 0;
 	}
 
 	shuffle() {
-		let len = this.cups.length;
-		let remove = [];
-		for (let i = 1; i < 4; i++) {
-			remove.push(this.cups[(this.current + i) % len]);
+		let remove1 = this.cups[this.current];
+		let remove2 = this.cups[remove1];
+		let remove3 = this.cups[remove2];
+		let insertAt = this.current === 1 ? this.cups.length - 1 : this.current - 1;
+		while (insertAt === remove1 || insertAt === remove2 || insertAt === remove3) {
+			insertAt = insertAt === 1 ? this.cups.length - 1 : insertAt - 1
 		}
-		let val = this.cups[this.current];
-		let dest = val === 1 ? len : val - 1;
-		while (remove.includes(dest)) { dest = dest === 1 ? len : dest - 1; }
-		dest = this.cups.findIndex(x => x === dest);
-		if (dest < this.current) {
-			for (let i = this.current + 3; i > dest + 3; i--) { this.cups[i % len] = this.cups[(i - 3) % len]; }
-			dest++;
-			for (let i = 0; i < 3; i++) { this.cups[(dest + i) % len] = remove[i]; }
-			this.current = (this.current + 4) % len;
-		} else {
-			for (let i = this.current + 1; i < dest - 2; i++) { this.cups[i] = this.cups[i + 3]; }
-			for (let i = 0; i < 3; i++) { this.cups[dest - i] = remove[2 - i]; }
-			this.current = (this.current + 1) % len;
-		}
+		this.cups[this.current] = this.cups[remove3];
+		this.cups[remove3] = this.cups[insertAt];
+		this.cups[insertAt] = remove1;
+		this.current = this.cups[this.current];
 		this.shuffles++;
 	}
 
-	toString() {
-		let txt = `Shuffle ${this.shuffles}: `;
-		for (let i = 0; i < this.cups.length; i++) {
-			if (i === (this.current + 1) % this.cups.length) { txt += "["; }
-			// if (this.cups[i] > 13 && this.cups[i] < 18) { txt += '*'; }
-			if (i === this.current) { txt += `(${this.cups[i]})`; } else { txt += `${this.cups[i]}` }
-			if (i === (this.current + 3) % this.cups.length) { txt += "] " } else { txt += " "; }
-			// if (i < this.cups.length - 1 && this.cups[i] < 100 && this.cups[i + 1] > 500000) { txt += "... "; }
-			// if (i < this.cups.length - 1 && this.cups[i] < 100 && this.cups[i + 1] > 500000) { console.log((this.cups[i + 1] - this.cups[i]) % 4); }
-		}
-		return txt;
-	}
-
 	key() {
-		let b = this.cups.findIndex(x => x === 1);
-		// let s = this.cups.slice(b).concat(this.cups.slice(0, b));
-		// return this.cups[this.current] + s.join('');
-		return this.cups.slice(b + 1).concat(this.cups.slice(0, b)).join('');
+		let next = this.cups[1], s = "";
+		for (let i = 1; i < this.cups.length - 1; i++) {
+			s += next;
+			next = this.cups[next];
+		}
+		return s;
 	}
 }
 
 export class S23a extends Solver {
 	shuffle(state) {
 		if (state.shuffles > 9999999) {
-			let i = state.cups.findIndex(x => x === 1);
-			i = state.cups[(i + 1) % 1000000] * state.cups[(i + 2) % 1000000];
-			this.setState({ result: i });
+			let i = state.cups[1];
+			this.setState({ result: i * state.cups[i] });
 			return;
 		}
-		for (let i = 0; i < 100; i++) { state.shuffle(); }
+		for (let i = 0; i < 100000; i++) { state.shuffle(); }
 		this.setState({ part2: state });
 		setTimeout(() => this.shuffle(state), 1);
 	}
 
 	solve(input) {
-		input = "389125467";
+		// input = "389125467";
 		let part1 = new Cups(input);
 		for (let i = 0; i < 100; i++) { part1.shuffle(); }
 
-		let part2 = new Cups(input);
-		while (part2.cups.length < 1000000) {
-			part2.cups.push(part2.cups.length + 1);
-		}
+		let part2 = new Cups(input, 1000000);
 
 		this.setState({ part1: part1.key(), part2: part2 });
 		setTimeout(() => this.shuffle(part2), 1);
